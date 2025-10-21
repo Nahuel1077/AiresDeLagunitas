0b72081c60aabfd18955228b6e33f4709a3cde44
Nahuel1077 <nahuh08@gmail.com>
2025-10-21 14:56:27 -0300

    fixed links

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -1,6 +1,6 @@
 'use client'
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function Banner(){
 
@@ -9,7 +9,7 @@ export default function Banner(){
     return(
         <aside className={`fixed bottom-0 w-full sm:w-[50vw] justify-self-center z-20 h-[70px] bg-black text-white border-black text-center text-xl animate__animated animate__slideInUp ${visible? "absolute" : "hidden"}`}>
             <h1>¡Promo de estreno!🔥 Tiempo limitado⏳</h1>
-            <Link href="#">Click aquí</Link>
+            <Link href="/tarifas/#promo">Click aquí</Link>
             <button onClick={() => {setVisible(!visible)}} className="absolute bottom-[55px] left-[95vw]  sm:bottom-10 sm:left-[48vw]">✖</button>
         </aside>
     )
